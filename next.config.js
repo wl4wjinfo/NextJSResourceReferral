0520a230bd0a399ce52dcd1ba728fa21e71e7ab6
tdaentbluv <tdaentbluv@gmail.com>
2025-01-15 02:25:59 -0500

    chore: add custom server and update build config for production

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: process.env.NODE_ENV === 'production',
@@ -56,9 +57,9 @@ const nextConfig = {
     ];
   },
   experimental: {
-    serverActions: true
+    serverActions: true,
   },
-  output: 'standalone'
+  distDir: '.next'
 };
 
 module.exports = nextConfig;
